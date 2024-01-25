61219511015ccbf75d659218df9b80d9f4f736ef
Farhaan Kaleem <farhaankaleem@farhaans-mbp.home>
2024-01-25 16:11:00 +0000

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -3,7 +3,18 @@
 interface Friend {
     name: string;
     phone: string;
-    age: number
+    age: number;
+}
+
+interface Colleague {
+    name: string;
+    department: string;
+    contact: Contact;
+}
+
+interface Contact {
+    email: string;
+    extension: number;
 }
 
 const friend1: Friend = {
@@ -22,7 +33,7 @@ const friend1: Friend = {
   console.log(friends[1]);
   
   //   -------------------
-  const colleague1 = {
+  const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -31,7 +42,7 @@ const friend1: Friend = {
     },
   };
   
-  const colleague2 = {
+  const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -40,7 +51,7 @@ const friend1: Friend = {
     },
   };
   
-  const colleague3 = {
+  const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
