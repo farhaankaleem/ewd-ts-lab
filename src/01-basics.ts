2d69a41a00b804b4e694af484590401de2c79830
Farhaan Kaleem <farhaankaleem@farhaans-mbp.home>
2024-01-25 16:12:44 +0000

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -17,6 +17,11 @@ interface Contact {
     extension: number;
 }
 
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+  }
+
 const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
@@ -59,7 +64,7 @@ const friend1: Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+  const colleagues: ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
